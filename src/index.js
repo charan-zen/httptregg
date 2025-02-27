258fd40be312f44b6dd48899b16b5082450fb95f
charan-zen <gorrecharansaiintern@zentience.co>
2025-02-27 16:21:23 +0530

    create azure function http add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const { app } = require('@azure/functions');
+const fetch = require('node-fetch');
 
-app.setup({
-    enableHttpStream: true,
-});
+module.exports = async function (context, req) {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await response.json();
+
+        context.res = {
+            status: 200,
+            body: data,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: { error: "Failed to fetch data", details: error.message }
+        };
+    }
+};
